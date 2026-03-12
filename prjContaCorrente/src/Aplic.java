66e4f5d26e48f5aabc595cbb2717039e09d9b089
EdsssBarros <edsbarrosjr@gmail.com>
2026-03-12 20:35:18 -0300

    atualização

---
+++ prjContaCorrente/src/Aplic.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+/**
+ *
+ * @author edson.barros
+ */
+public class Aplic {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        int num, opcao;
+        double valSaldo, ValSaq, ValDep;
+        
+        System.out.println("/n/nDigite o número da conta: ");
+        num = entrada.nextInt();
+        
+        System.out.println("/n/nDigite o valor do saldo da conta: ");
+        valSaldo = entrada.nextDouble();
+        
+        ContaCorrente objContaCorrente1 = new ContaCorrente(num, valSaldo);
+        
+        do{
+            
+            hud
+            
+            opcao = entrada.nextint();
+            
+            System.out.println("\n\nNumero da conta corrente: " + );
+            
+        } While ()
+        
+    }
+}
